0a9920f4681725c8862d7e76479cff0f026fad47
Ben Jones <ben@bnjns.uk>
2017-02-08 01:15:09 +0000

    Add support for sending standardised AJAX responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
     
     namespace App\Http\Controllers;
     
+    use Illuminate\Contracts\Auth\Factory;
     use Illuminate\Foundation\Bus\DispatchesJobs;
     use Illuminate\Http\Request;
     use Illuminate\Pagination\LengthAwarePaginator;
@@ -25,5 +26,37 @@
             if($paginator->count() == 0 && !is_null(Input::get('page')) && (int) Input::get('page') != 1) {
                 return redirect()->route(Route::current()->getName(), Input::except('page') + ['page' => 1]);
             }
+        }
+        
+        /**
+         * Create a response for an AJAX request.
+         * @param       $text
+         * @param int   $status
+         * @param array $data
+         * @param array $headers
+         * @return \Illuminate\Http\JsonResponse
+         */
+        protected function ajaxResponse($text, $status = 200, array $data = [], array $headers = [])
+        {
+            $data = array_merge($data, [($status == 200 ? 'response' : 'error') => $text]);
+            
+            return response()->json($data, $status, $headers);
+        }
+        
+        /**
+         * Create an AJAX error response.
+         * @param     $errorText
+         * @param int $status
+         * @return \Illuminate\Http\JsonResponse
+         */
+        protected function ajaxError($errorCode, $status = 422, $errorText = null)
+        {
+            $data = [
+                'error_code' => $errorCode,
+                '__error'    => true,
+            ];
+            $text = $errorText ?: trans('errors.' . $errorCode);
+            
+            return $this->ajaxResponse($text, $status, $data);
         }
     }
