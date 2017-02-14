c8e59da2f1e0cbe2430da1cbd0b927eff8f1ee07
Ben Jones <ben@bnjns.uk>
2017-02-14 22:59:47 +0000

    Add functionality for video gallery

--- routes/web/media.php
+++ routes/web/media.php
@@ -2,14 +2,30 @@
     Route::group([
         'prefix' => 'media',
     ], function () {
-        // Index
-        Route::get('images', [
-            'as'   => 'media.images.index',
-            'uses' => 'Media\ImageController@index',
-        ]);
-        // Album
-        Route::get('images/album/{id}', [
-            'as'   => 'media.images.album',
-            'uses' => 'Media\ImageController@album',
-        ]);
+        // Images
+        Route::group([
+            'prefix' => 'images',
+        ], function () {
+            Route::get('', [
+                'as'   => 'media.images.index',
+                'uses' => 'Media\ImageController@index',
+            ]);
+            Route::get('album/{id}', [
+                'as'   => 'media.images.album',
+                'uses' => 'Media\ImageController@album',
+            ]);
+        });
+        // Videos
+        Route::group([
+            'prefix' => 'videos',
+        ], function () {
+            Route::get('', [
+                'as'   => 'media.videos.index',
+                'uses' => 'Media\VideoController@index',
+            ]);
+            Route::get('{id}', [
+                'as'   => 'media.videos.show',
+                'uses' => 'Media\VideoController@show',
+            ]);
+        });
     });
