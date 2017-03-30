6338e14435a07e5e4a1f4317ab5e9d9622f252f5
Ben Jones <ben@bnjns.uk>
2017-03-30 16:12:11 +0100

    Update link to the asset register

--- resources/views/equipment/assets.blade.php
+++ resources/views/equipment/assets.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
     <h1 class="page-header">@yield('title')</h1>
-    <iframe width='100%' height='700' frameborder='0' src='https://docs.google.com/spreadsheet/pub?key=0AlW0Joq6HFaIdGtWb3MzUWNPWi1BMGp4b3ctRkQ3UXc&output=html&widget=true'></iframe>
+    <iframe width="100%" height="700" frameborder="0" src="https://docs.google.com/spreadsheets/d/19oMod3uCJp51h_BfpCy0sprKueOWOxBXSmI1PcIm19I/pubhtml?widget=false&headers=false&chrome=false&gid=0"></iframe>
 @endsection
