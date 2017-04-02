1b088a9043ed7e9ac84a70b4fdfaf7fff89df0b0
Ben Jones <ben@bnjns.uk>
2017-04-02 14:21:06 +0100

    Fix menu bar in mobile view

--- resources/views/app/includes/menu.blade.php
+++ resources/views/app/includes/menu.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default wrapper">
     <div class="container-fluid">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-main">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="fa fa-bars"></span>
             </button>
