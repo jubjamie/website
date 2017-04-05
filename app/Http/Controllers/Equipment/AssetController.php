2e0f4506d0cca498cc5eb7765532850fa507d91f
Ben Jones <ben@bnjns.uk>
2017-04-05 19:15:00 +0100

    Add authorisation to asset register

--- app/Http/Controllers/Equipment/AssetController.php
+++ app/Http/Controllers/Equipment/AssetController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Equipment;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Gate;
 
 class AssetController extends Controller
 {
     /**
      * View the asset register.
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function view()
     {
+        if(!Gate::allows('members.strict')) {
+            throw new AuthorizationException();
+        }
+        
         return view('equipment.assets.view');
     }
 }
