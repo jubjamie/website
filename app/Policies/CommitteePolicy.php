820f29a14f158304495e8ba492dcc66965b0228c
Ben Jones <ben@bnjns.uk>
2017-04-10 00:18:46 +0100

    Update packages and dependencies

--- app/Policies/CommitteePolicy.php
+++ app/Policies/CommitteePolicy.php
@@ -9,35 +9,32 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class CommitteePolicy
 {
     use HandlesAuthorization;
-
-        /**
+    
+    /**
      * Determine whether the user can create committeeRoles.
-     *
-     * @param  \App\User  $user
+     * @param  \App\User $user
      * @return mixed
      */
     public function create(User $user)
     {
         return $user->isAdmin();
     }
-
+    
     /**
      * Determine whether the user can update the committeeRole.
-     *
-     * @param  \App\User  $user
-     * @param  \App\CommitteeRole  $committeeRole
+     * @param  \App\User          $user
+     * @param  \App\CommitteeRole $committeeRole
      * @return mixed
      */
     public function update(User $user, CommitteeRole $committeeRole)
     {
         return $user->isAdmin();
     }
-
+    
     /**
      * Determine whether the user can delete the committeeRole.
-     *
-     * @param  \App\User  $user
-     * @param  \App\CommitteeRole  $committeeRole
+     * @param  \App\User          $user
+     * @param  \App\CommitteeRole $committeeRole
      * @return mixed
      */
     public function delete(User $user, CommitteeRole $committeeRole)
