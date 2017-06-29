206af677465abe0e085d156d23ad071b58dafe75
Ben Jones <ben@bnjns.uk>
2017-06-30 01:52:06 +0200

    Add trait to recursively delete a directory

---
+++ app/Traits/DeletesDirectory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+trait DeletesDirectory
+{
+    /**
+     * Recursively delete a directory and all the files within.
+     * @param string $dir
+     * @return bool
+     */
+    public function rmdir($dir)
+    {
+        if(file_exists($dir) === false) {
+            return false;
+        }
+        
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        
+        foreach($files as $fileinfo) {
+            if($fileinfo->isDir()) {
+                if(rmdir($fileinfo->getRealPath()) === false) {
+                    return false;
+                }
+            } else {
+                if(unlink($fileinfo->getRealPath()) === false) {
+                    return false;
+                }
+            }
+        }
+        
+        return rmdir($dir);
+    }
+}
